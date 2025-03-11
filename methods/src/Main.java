83d1ee1f2ae84b2b4bb60f77332303931295b8e0
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-11 20:34:33 +0300

    methods1 update

--- methods/src/Main.java
+++ methods/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, methods!");
+        numberSearch();
+    }
+    public static  void numberSearch() {
+        int[] sayilar = new int [] {1,2,5,7,9,0};
+
+        int aranacak = 0;
+        boolean varMi = false;
+
+        for (int sayi : sayilar) {
+            if (sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+        if (varMi) {
+            messageGive("Sayı mevcuttur.." + aranacak);
+        } else {
+            messageGive("Sayı mevcut değildir.." + aranacak);
+        }
+    }
+    public static void messageGive(String mesaj){
+        System.out.println(mesaj);
     }
 }

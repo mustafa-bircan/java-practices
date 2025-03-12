fab94aca59ad91ece8da92075bde4e73f562ed03
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-12 22:06:09 +0300

    Work was done Overloading

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        FourOperations fourOperations  = new FourOperations();
+        System.out.println(fourOperations.sum(4,6));
+        fourOperations.sum(4,5,6);
+    }
+}
+
+//Claslar ortak operasyonları veya attributeleri tutmak için kullanılır.

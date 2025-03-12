6f3c15e96e98eba69560afb6426b685898b5a84c
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-12 21:02:42 +0300

    Work was done with getter & setter

--- classesWithAttributes/src/ProductManager.java
+++ classesWithAttributes/src/ProductManager.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class ProductManager {
+    public void Add(Product product) {
+        //JDBC
+        System.out.println("Ürün eklendi " + product.getName());
+    }
+
+    public void Add2(int id, String name, String description, int stockAmount, double price){
+
+    }
 }

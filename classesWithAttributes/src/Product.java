f8cc5e78eb1763ec790b98898ab84601ce32c83d
mustafa-bircan <msbrtfcn@gmail.com>
2025-03-12 21:56:55 +0300

    Work was done with constructor

--- classesWithAttributes/src/Product.java
+++ classesWithAttributes/src/Product.java
@@ -8,6 +8,17 @@ public class Product {
     private String renk;
     private String kod;
 
+    //clasların default constructorları vardır.
+    public Product(int id, String name, String description, double price, int stockAmount, String renk) {
+        System.out.println("Constructor started!!!");
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockAmount = stockAmount;
+        this.renk = renk;
+    }//productı bu parametreleri verirsem kullaanbilirsin demek.
+
     public int getId() {
         return id;
     }

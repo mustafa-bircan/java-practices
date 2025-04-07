3ef3f9ba6b3467b150c10919efe001de053ba443
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-07 19:26:26 +0300

    Added exception handling for general errors and printed the total sum

--- readingFileDemo/src/Main.java
+++ readingFileDemo/src/Main.java
@@ -1,18 +1,25 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         BufferedReader reader = null;
+        int total = 0;
         try {
              reader = new BufferedReader(
-                    new FileReader("C:\\Users\\msbrt\\java-practices\\readingFileDemo\\src\\numbers.txt")
-            );
+                    new FileReader("C:\\Users\\msbrt\\java-practices\\readingFileDemo\\src\\numbers.txt"));
+
+                     String line = null;
+                     while ((line = reader.readLine()) != null) {
+                            total += Integer.valueOf(line);
+                     }
+            System.out.println("Toplam= " + total);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }finally {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
             try {
                 reader.close();
             }catch (Exception e) {

9c45075c38a0dd36f4d3916dba2979a3eeb9ee86
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-07 16:28:47 +0300

    Created default package for testing package usage

---
+++ packagesDemo/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adınız: ");
+
+         String isim = scanner.nextLine();
+        System.out.println("Merhaba " + isim);
+    }
+}

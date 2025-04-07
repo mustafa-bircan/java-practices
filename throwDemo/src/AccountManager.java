1bbec2b16fbf63ea2ffecf757391818d7b997b56
mustafa-bircan <msbrtfcn@gmail.com>
2025-04-07 21:15:09 +0300

    Add deposit and withdraw methods with balance check and exception handling

---
+++ throwDemo/src/AccountManager.java
@@ -0,0 +1,17 @@
+public class AccountManager {
+   private double balance;
+   public void deposit(double amount) {
+       balance += amount;
+   }
+   public void withdraw(double amount) throws Exception {
+       if (balance >= amount) {
+           balance = getBalance() - amount;
+       }else {
+           throw new Exception("Bakiye Yetersiz");
+       }
+   }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ throwDemo/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        AccountManager accountManager = new AccountManager();
+        System.out.println("Hesap = " + accountManager.getBalance());
+        accountManager.deposit(100);
+        System.out.println("Hesap = " + accountManager.getBalance());
+        try {
+            accountManager.withdraw(90);
+        }catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        System.out.println("Hesap = " + accountManager.getBalance());
+
+        try {
+            accountManager.withdraw(20);
+        }catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        System.out.println("Hesap = " + accountManager.getBalance());
+
+    }
+}
